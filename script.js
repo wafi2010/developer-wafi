7c778a942f97e8a0c40bd6f8bf31b53193a16112
Wafi Hasan <wafihasan2010@gmail.com>
2025-01-25 10:52:50 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+  // Sticky Navbar on Scroll
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+
+    // Scroll-up button visibility
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  // Scroll-up button click
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  // Smooth scroll for menu items
+  $(".navbar .menu li a").click(function () {
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  // Toggle Navbar
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
+  // Typing Text Animation
+  var typed = new Typed(".typing", {
+    strings: [
+      "Junior Web Developer",
+      "Frontend Developer",
+      "Co-Founder DevBros Technology - DbT",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+
+
+
+  // Owl Carousel Initialization
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
+});
